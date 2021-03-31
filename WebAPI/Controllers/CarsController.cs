5b7217a384be29e6ab1da711361bd1a1a89cde14
Hakan Avci <hkn.avci@hotmail.com>
2021-03-31 21:11:12 +0300

    11.Gün Ödev 1

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _carService.GetAll();
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            return BadRequest(result);
+        }
+
+        [HttpPost("CarAdd")]
+        public IActionResult CarAdd(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            else
+                return BadRequest(result);
+        }
+    }
+}
